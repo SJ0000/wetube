5e93a73c31c4945c4c9a3134a15ab3139cf84f98
SUNGJIN.KANG <slaayer60@gmail.com>
2020-06-18 23:04:48 +0700

    ~ 2.14
    Using PUG ( View Engine )

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Cloning Youtube with Varmilla and NodeJS
+
+Model : Data
+View : How does the data look
+Controller : Function that looks for the data
--- app.js
+++ app.js
@@ -3,20 +3,17 @@ import morgan from "morgan"; // Logger
 import helmet from "helmet"; // node.js 보안
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
-const handleHome = (req, res) => {
-  res.send("Hello from home");
-};
-
-const handleProfile = (req, res) => {
-  res.send("YOU ARE ON MY PROFILE");
-};
-
 // app.use(myMiddleware): 이 함수 이후에 추가되는 Route 들에 myMiddleware를 추가한다
 // !! 순서가 중요함
+
+app.set("view engine", "pug");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -24,12 +21,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-// http://localhost:4000/
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter); // /user에 접속하면 userRouter를 사용하겠다는 뜻
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter); // /user에 접속하면 userRouter를 사용하겠다는 뜻
+app.use(routes.videos, videoRouter);
 
 //누군가 app.js를 불러올때 app 객체를 주겠다는 뜻
 export default app;
