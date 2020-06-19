05665baa0be1a2c816dc1f80497151a5b3de5076
SUNGJIN.KANG <slaayer60@gmail.com>
2020-06-19 23:57:49 +0700

    ~ 2.17
    pug

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-import express from "express";
-import morgan from "morgan"; // Logger
-import helmet from "helmet"; // node.js 보안
-import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import express from "express";
+import globalRouter from "./routers/globalRouter";
+import helmet from "helmet"; // node.js 보안
+import routes from "./routes";
+import morgan from "morgan"; // Logger
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
 // app.use(myMiddleware): 이 함수 이후에 추가되는 Route 들에 myMiddleware를 추가한다
 // !! 순서가 중요함
+app.use(helmet());
 
 app.set("view engine", "pug");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware); // View (.pug) 에서 Controller 의 변수를 사용할 수 있게 함
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter); // /user에 접속하면 userRouter를 사용하겠다는 뜻
---
+++ middlewares.js
@@ -0,0 +1,7 @@
+import routes from "./routes";
+// View (.pug)에서 Controller의 변수를 사용할 수 있게 함
+export const localsMiddleware = (req, res, next) => {
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  next();
+};
