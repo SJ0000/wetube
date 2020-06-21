7aa41683ad3b66cc077f6281e4ed7b700e6a0be2
SUNGJIN.KANG <slaayer60@gmail.com>
2020-06-21 23:31:04 +0700

    ~ 2.25

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Cloning Youtube with Varmilla and NodeJS
 Model : Data
 View : How does the data look
 Controller : Function that looks for the data
+
+## Pages :
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ db.js
@@ -0,0 +1,43 @@
+// Fake DB
+
+export const videos = [
+  {
+    id: 532414,
+    title: "Video Awesome 1",
+    description: "This is very Awesome Video 1",
+    views: 12324234,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Sungjin",
+      email: "alpha@beta.com",
+    },
+  },
+  {
+    id: 463452,
+    title: "Video Awesome 2",
+    description: "This is very Awesome Video 2",
+    views: 12324234,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Sungjin",
+      email: "alpha@beta.com",
+    },
+  },
+  {
+    id: 2342,
+    title: "Video Awesome 3",
+    description: "This is very Awesome Video 3",
+    views: 12324234,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Sungjin",
+      email: "alpha@beta.com",
+    },
+  },
+];
