ee02514e4e641c520c03849c997c7ec6fbca6548
SUNGJIN.KANG <slaayer60@gmail.com>
2020-06-25 00:38:13 +0700

    ~ 3.10
    Video delete & update using mongoose

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,20 +2,28 @@ import express from "express";
 import routes from "../routes";
 import {
   videoDetail,
-  editVideo,
   deleteVideo,
   getUpload,
   postUpload,
+  getEditVideo,
+  postEditVideo,
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload); // post로 upload하면 uploadVideo 라는 middleware를 거친다
 
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
